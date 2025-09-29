3423c7eb5c70113350292f23edf00a6050117cde
Leonardo Bento <sismico@gmail.com>
2025-09-28 21:53:44 -0300

    Add Docker Compose and Spring Project

---
+++ kafka-spring/src/main/kotlin/com/sismico/kafka_spring/KafkaSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.sismico.kafka_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaSpringApplication>(*args)
+}
---
+++ kafka-spring/src/test/kotlin/com/sismico/kafka_spring/KafkaSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sismico.kafka_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
